5bc9da122974ea8b4584454a0a8afdbb378505a7
Liang Li <liliang.imut@gmail.com>
2026-07-31 10:29:46 +0800

    brew: v0.3.0 (OpenClaw bridge release)

--- HomebrewFormula/tagit.rb
+++ HomebrewFormula/tagit.rb
@@ -1,28 +1,28 @@
 class Tagit < Formula
   desc "Daemon-first orchestrator for coding-agent CLIs (claude, codex, ...)"
   homepage "https://github.com/liliang-cn/tagit"
-  version "0.2.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/liliang-cn/tagit/releases/download/v0.2.0/tagit_darwin_arm64.tar.gz"
-      sha256 "9a4d778366db517ec1bb8900acf7005091a062054850c4564b4f1c232a98d7a5"
+      url "https://github.com/liliang-cn/tagit/releases/download/v0.3.0/tagit_darwin_arm64.tar.gz"
+      sha256 "0a77804e7c1a0eb3baa83abc9742c6c27ab41a3b80c3ed8f954010d551ddc940"
     end
     on_intel do
-      url "https://github.com/liliang-cn/tagit/releases/download/v0.2.0/tagit_darwin_amd64.tar.gz"
-      sha256 "fe831550023f280c5c047e89c99b9237d2022707cdafe92fdff62792f3a5fa25"
+      url "https://github.com/liliang-cn/tagit/releases/download/v0.3.0/tagit_darwin_amd64.tar.gz"
+      sha256 "5ceb9f5465625b69916ee077b9e2f523c7fee34f11a622f8d7e80e2643b27d06"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/liliang-cn/tagit/releases/download/v0.2.0/tagit_linux_arm64.tar.gz"
-      sha256 "d9a3b9acd9a9fd6f4593207f013dea501f4b80dca0498917b2b6165bbabd626a"
+      url "https://github.com/liliang-cn/tagit/releases/download/v0.3.0/tagit_linux_arm64.tar.gz"
+      sha256 "d606949405c612bd2d23876eb69eca86438cf7e1a69cf39e00b4573efe271ab4"
     end
     on_intel do
-      url "https://github.com/liliang-cn/tagit/releases/download/v0.2.0/tagit_linux_amd64.tar.gz"
-      sha256 "f33d9fb79d0438fa4543c97ef41acad05080ff06a72290e58a619bfd026a5644"
+      url "https://github.com/liliang-cn/tagit/releases/download/v0.3.0/tagit_linux_amd64.tar.gz"
+      sha256 "859435e35a63cf2981fb3bdcb15e8cfd094ac867c7ff6a0015c1229deb587245"
     end
   end
 
